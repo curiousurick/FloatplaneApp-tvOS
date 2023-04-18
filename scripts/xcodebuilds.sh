fa261a21e9d8e46a8a398d41a795129b74efb6fb
curiousurick@icloud.com <curiousurick@icloud.com>
2023-04-18 00:38:02 -0700

    Renaming folders from FloatplaneApp to FloatplaneApp-tvOS

--- scripts/xcodebuilds.sh
+++ scripts/xcodebuilds.sh
@@ -5,6 +5,6 @@ base_path=$(git rev-parse --show-toplevel);
 
 set -e
 
-xcodebuild build -project FloatplaneApp-App/FloatplaneApp.xcodeproj -scheme FloatplaneApp -destination 'platform=tvOS Simulator,name=Apple TV 4K (3rd generation),OS=16.1';
+xcodebuild build -project ${base_path}/FloatplaneApp-tvOS.xcodeproj -scheme FloatplaneApp-tvOS -destination 'platform=tvOS Simulator,name=Apple TV 4K (3rd generation),OS=16.1';
 
 exit 0
